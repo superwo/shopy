c36ea8c4949ec149febeadc3b8e389b6d5a525a1
avadarad <avadanii@tiscali.it>
2017-10-10 22:01:03 +0300

    made the menu-type items

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,21 @@
 $(function() {
+	var menuLinks = $('.menu-types__link');
+	menuLinks.hover(
+		function(event) {
+		var icon = $(this).find('.icon');
+		var text = icon.attr('class').split(' ')[1].split('--')[1];
+		var newClass = 'icon--' + text + '-active';
+		icon.removeClass();
+		icon.addClass('icon');
+		icon.addClass(newClass);
+	},
+	function(event) {
+		var icon = $(this).find('.icon');
+		var text = icon.attr('class').split(' ')[1].split('--')[1];
+		var newClass = 'icon--' + text.split('-')[0];
+		icon.removeClass();
+		icon.addClass('icon');
+		icon.addClass(newClass);
+	})
 
-	// Custom JS
-	
 });
